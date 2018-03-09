fdd3e3649cf2ff1b7431b1117063ff776804b41d
Lyn Nagara <lyn.nagara@gmail.com>
2018-03-09 15:37:08 -0800

    feat: Add list of errors

--- app/components/error-page.js
+++ app/components/error-page.js
@@ -3,17 +3,79 @@ import Component from '@ember/component';
 export default Component.extend({
     init() {
         this._super(...arguments);
-        this.errorList = [{text: 'decodeURIComponent error', action: uriError}]
+        this.errorList = getErrorList();
     },
 
     actions: {
         getErrorList() {
-            return this.get('errorList')
+            return this.get('errorList');
         }
     }
-
 });
+
+
+function getErrorList() {
+    return [
+        {
+            title: 'TypeError',
+            description: 'obj.attributeInvalid is not a function',
+            action: notAFunctionError
+        }, {
+            title: 'URIError',
+            description: 'URI malformed',
+            action: uriError
+        }, {
+            title: 'Uncaught TypeError',
+            description: 'Cannot read property \'f\' of null',
+            action: typeError
+        }, {
+            title: 'SyntaxError',
+            description: 'Unexpected identifier',
+            action: syntaxError
+        }, {
+            title: 'ReferenceError',
+            description: 'undefinedVariable is not defined',
+            action: referenceError
+        }, {
+            title: 'RangeError',
+            description: 'Parameter must be between 1 and 100',
+            action: rangeError
+        }, {
+            title: 'EvalError',
+            description: 'Hello',
+            action: evalError
+        }
+    ]
+}
+
+// error functions
+function notAFunctionError() {
+    var obj = {};
+    obj.invalidFunction();
+}
 
 function uriError() {
     decodeURIComponent('%');
+}
+
+function typeError() {
+    null.f();
+}
+
+function syntaxError() {
+    eval('foo bar');
+}
+
+function referenceError() {
+    var a = undefinedVariable;
+}
+
+function rangeError() {
+    throw new RangeError('Parameter must be between 1 and 100');
+}
+
+function evalError() {
+    setTimeout(() => {
+        throw new EvalError('Hello', 'someFile.js', 10);
+    }, 1000);
 }
