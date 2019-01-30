575cf062f146ab27e5af17b38808854b8679cf6e
Neil Manvar <neil@sentry.io>
2019-01-30 15:12:43 -0800

    Update with new Sentry SDK

--- app/app.js
+++ app/app.js
@@ -3,6 +3,14 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+  dsn: 'https://083642f6753749ffb0eea864a7c2ed27@sentry.io/302369',
+  integrations: [new Sentry.Integrations.Ember()],
+  release: config.EmberENV.release
+});
+
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
